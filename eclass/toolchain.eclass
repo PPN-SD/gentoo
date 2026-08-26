PPN-SD/gentoo

commit e9e6d8c367fdf02b14f9cb2a59489833975d1445

toolchain.eclass: drop support EAPI 7
should only supports EAPI=8 since rust was introduced in inherit.

Signed-off-by: Nicolas PARLANT <nicolas.parlant@parhuet.fr>
Part-of: https://github.com/gentoo/gentoo/pull/40954
Closes: https://github.com/gentoo/gentoo/pull/40954
Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -4,7 +4,7 @@
 # @ECLASS: toolchain.eclass
 # @MAINTAINER:
 # Toolchain Ninjas <toolchain@gentoo.org>
-# @SUPPORTED_EAPIS: 7 8
+# @SUPPORTED_EAPIS: 8
 # @BLURB: Common code for sys-devel/gcc ebuilds
 # @DESCRIPTION:
 # Common code for sys-devel/gcc ebuilds (and occasionally GCC forks, like
@@ -17,7 +17,7 @@ _TOOLCHAIN_ECLASS=1
 RUST_OPTIONAL="1"
 
 case ${EAPI} in
-	7|8) ;;
+	8) ;;
 	*) die "${ECLASS}: EAPI ${EAPI:-0} not supported" ;;
 esac
 
@@ -463,13 +463,8 @@ PDEPEND=">=sys-devel/gcc-config-2.11"
 # @ECLASS_VARIABLE: TOOLCHAIN_PATCH_SUFFIX
 # @DESCRIPTION:
 # Used to override compression used for for patchsets.
-# Default is xz for EAPI 8+ and bz2 for older EAPIs.
-if [[ ${EAPI} == 8 ]] ; then
+# Default is xz for EAPI 8+.
 	: "${TOOLCHAIN_PATCH_SUFFIX:=xz}"
-else
-	# Older EAPIs
-	: "${TOOLCHAIN_PATCH_SUFFIX:=bz2}"
-fi
 
 # @ECLASS_VARIABLE: TOOLCHAIN_SET_S
 # @DESCRIPTION:
@@ -766,11 +761,10 @@ do_gcc_gentoo_patches() {
 				fi
 			fi
 
-			local shopt_save=$(shopt -p nullglob)
+			local -
 			shopt -s nullglob
 			einfo "Applying musl patches ..."
 			eapply "${WORKDIR}"/musl/{,nocross/}*.patch
-			${shopt_save}
 		fi
 
 		#

commit 71d89ba657159589924517fb29ad14f005421587

toolchain.eclass: revert use of local - for nullglob
'local -' only restores single-letter shell options managed by set

Fixes: e9e6d8c367fdf02b14f9cb2a59489833975d1445
Signed-off-by: Nicolas PARLANT <nicolas.parlant@parhuet.fr>

eclass/toolchain.eclass
@@ -729,10 +729,11 @@ do_gcc_gentoo_patches() {
 				fi
 			fi
 
-			local -
+			local shopt_save=$(shopt -p nullglob)
 			shopt -s nullglob
 			einfo "Applying musl patches ..."
 			eapply "${WORKDIR}"/musl/{,nocross/}*.patch
+			${shopt_save}
 		fi
 
 		#